1f6ae1f3cf4d54e366fa8d5c363e7897ccea93da
Akhil Gupta <akhil.gupta.12817@gmail.com>
2024-03-16 18:16:28 +0530

    keyboardShortcuts-pinTab

--- clientside/public/background.js
+++ clientside/public/background.js
@@ -1,14 +1,27 @@
+
+// function that closes tabs on custom intervals
 function closeInactiveTabs() {
-    chrome.tabs.query({}, function(tabs) {
-        console.log(tabs);
-        const currentTime = new Date().getTime();
-        console.log(interval);
-        tabs.forEach(function(tab) {
-            const tabLastActiveTime = new Date(tab.lastAccessed).getTime();
-            if (currentTime - tabLastActiveTime > interval) {
-                chrome.tabs.remove(tab.id);
-            }
+    chrome.storage.sync.get(['customInterval'], function(result) {
+        const interval = result.customInterval || 100000000; // Default interval: 1000000000
+        chrome.tabs.query({}, function(tabs) {
+            const currentTime = new Date().getTime();
+            tabs.forEach(function(tab) {
+                const tabLastActiveTime = new Date(tab.lastAccessed).getTime();
+                if (currentTime - tabLastActiveTime > interval * 60 * 1000) {
+                    chrome.tabs.remove(tab.id);
+                }
+            });
         });
     });
 }
-setInterval(closeInactiveTabs, 100000);
+setInterval(closeInactiveTabs, 600000); 
+
+// function to pin the active tab
+chrome.commands.onCommand.addListener(function(command) {
+    if (command === "pinTab") {
+      chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+        const tabId = tabs[0].id;
+        chrome.tabs.update(tabId, { pinned: true });
+      });
+    }
+  });
